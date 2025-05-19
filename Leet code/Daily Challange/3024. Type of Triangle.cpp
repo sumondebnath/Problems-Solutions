6d0642f52b3bfbd8aff16ad33dbe2a54b9dc6f85
Sumon Debnath <debnathsumon1708@gmail.com>
2025-05-19 10:15:07 +0600

    leetcode daily challange problem and solution.

---
+++ Leet code/Daily Challange/3024. Type of Triangle.cpp
@@ -0,0 +1,19 @@
+/*
+
+*/
+
+class Solution {
+public:
+    string triangleType(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+        int a = nums[0];
+        int b = nums[1];
+        int c = nums[2];
+
+        if(a+b <= c) return "none";
+        if(a==b && a==c && b==c) return "equilateral";
+        else if(a==b|| a==c || b==c) return "isosceles";
+        else return "scalene";
+    }
+};
