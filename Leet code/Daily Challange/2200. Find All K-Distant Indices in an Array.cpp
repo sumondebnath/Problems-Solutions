32d588e6914a3a6e7a2ab742f2a106a33d52e93c
Sumon Debnath <debnathsumon1708@gmail.com>
2025-06-25 00:53:16 +0600

    LeetCode daily challange problem and solutions.

---
+++ Leet code/Daily Challange/2200. Find All K-Distant Indices in an Array.cpp
@@ -0,0 +1,20 @@
+/*
+
+*/
+
+class Solution {
+public:
+    vector<int> findKDistantIndices(vector<int>& nums, int key, int k) {
+        vector<int> arr;
+
+        for(int i=0; i<nums.size(); i++){
+            for(int j=0; j<nums.size(); j++){
+                if(abs(i-j)<=k && nums[j]==key){
+                    arr.push_back(i);
+                    break;
+                }
+            }
+        }
+        return arr;
+    }
+};
