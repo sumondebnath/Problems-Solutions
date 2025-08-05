018ff5b23bdf1a223580d8f96e7bbd3a848be8fb
Sumon Debnath <debnathsumon1708@gmail.com>
2025-08-06 04:11:25 +0600

    LeetCode Daily challange problem and solution

---
+++ Leet code/Daily Challange/3477. Fruits Into Baskets II.cpp
@@ -0,0 +1,26 @@
+/*
+
+*/
+
+class Solution {
+public:
+    int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
+        int n = fruits.size();
+        int count = 0;
+
+        for(int i=0; i<n; i++){
+            bool flag = true;
+            for(int j=0; j<n; j++){
+                if(fruits[i] <= baskets[j]){
+                    baskets[j] = -1;
+                    flag = false;
+                    break;
+                }
+            }
+
+            if(flag) count++;
+        }
+
+        return count;
+    }
+};
