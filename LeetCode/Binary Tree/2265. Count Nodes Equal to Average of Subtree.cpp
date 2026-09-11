4f6ed6d744f12c51d81788a9633e4facfdfc1dd0
Sumon Debnath <debnathsumon1708@gmail.com>
2026-09-11 17:57:17 +0600

    added daily challange problem and solutions : 2265. Count Nodes Equal to Average of Subtree

---
+++ LeetCode/Binary Tree/2265. Count Nodes Equal to Average of Subtree.cpp
@@ -0,0 +1,25 @@
+/*
+
+*/
+
+class Solution {
+public:
+    int count = 0;
+    pair<int, int> postOrder(TreeNode* root){
+        if(root == NULL) return {0, 0};
+
+        pair<int, int> left = postOrder(root->left);
+        auto right = postOrder(root->right);
+
+        int nodeSum = left.first + right.first + root->val;
+        int nodeCount = left.second + right.second + 1;
+
+        if(root->val == (nodeSum / nodeCount)) count++;
+
+        return {nodeSum, nodeCount};
+    }
+    int averageOfSubtree(TreeNode* root) {
+        auto val = postOrder(root);
+        return count;
+    }
+};
